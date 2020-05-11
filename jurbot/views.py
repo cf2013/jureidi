489c06c4c050ada79ca2ef4830c2b08bdaea84c6
cf2013 <carlosfrancisco.rg@gmail.com>
2020-05-10 19:52:39 -0500

    using new envvars

--- jurbot/views.py
+++ jurbot/views.py
@@ -1,30 +1,23 @@
 from django.http import HttpResponse
 import requests
 import twilio
+import os
+from twilio.rest import Client
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def bot(request):
-    incoming_msg = request.values.get('Body', '').lower()
-    resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
-    if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
-    return HttpResponse(str(resp))
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+    .create(
+    media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
+    from_='whatsapp:+14155238886',
+    body="It's taco time!",
+    to='whatsapp:+5214426778033'
+    )
+    print(message.sid)
