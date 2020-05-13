fcbf25f1089041d441bd28341c1ad99ceaaf5fc6
cf2013 <carlosfrancisco.rg@gmail.com>
2020-05-13 00:40:02 -0500

    read message

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import requests
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return HttpResponse('200')
 
 
 def db(request):
--- jurbot/views.py
+++ jurbot/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.http import HttpRequest
 import requests
 import twilio
 import os
 from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt
-
+import json
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -13,18 +14,23 @@ def index(request):
 def bot(request):
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    body_unicode = request.body.decode('utf-8')
+    from urllib.parse import unquote
+    unq = unquote(body_unicode)
     #check id client
-    
-
-
+    if request:
+        ans=str(unq)
+    else:
+        ans='source'
     #answer back
     client = Client(account_sid, auth_token)
     message = client.messages \
     .create(
     from_='whatsapp:+14155238886',
-    body=str(HttpRequest.POST),
+    body=ans,
     to='whatsapp:+5214426778033'
     )
     print(message.sid)
     # Return an HHTPResponse as Django expects a response from the view
     return HttpResponse(status=200)
+
