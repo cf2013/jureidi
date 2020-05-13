ccf4402ed0f990cd473385deadb9ad2ffd9adfbc
cf2013 <carlosfrancisco.rg@gmail.com>
2020-05-12 22:15:59 -0500

    send back request body

--- jurbot/views.py
+++ jurbot/views.py
@@ -21,9 +21,8 @@ def bot(request):
     client = Client(account_sid, auth_token)
     message = client.messages \
     .create(
-    media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
     from_='whatsapp:+14155238886',
-    body=str(request),
+    body=str(request.values['Body']),
     to='whatsapp:+5214426778033'
     )
     print(message.sid)
