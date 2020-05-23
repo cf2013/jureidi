20cfb4d29e39fcbc22203fede8af02ffc47b2272
cf2013 <carlosfrancisco.rg@gmail.com>
2020-05-23 10:49:15 -0500

    update to local

--- jurbot/views.py
+++ jurbot/views.py
@@ -7,36 +7,46 @@ from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt
 import json
 import psycopg2
+from .models import Clients
+from urllib.parse import unquote
+from urllib.parse import urlencode
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def checkUser(id):
-    DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require') 
-
-
-@csrf_exempt
-def bot(request):
+def botReply(ans):
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    body_unicode = request.body.decode('utf-8')
-    from urllib.parse import unquote
-    unq = unquote(body_unicode)
-    #check id client
-    if request:
-        ans=str(unq)
-    else:
-        ans='source'
+
     #answer back
     client = Client(account_sid, auth_token)
     message = client.messages \
     .create(
-    from_='whatsapp:+14155238886',
-    body=ans,
-    to='whatsapp:+5214426778033'
+        from_='whatsapp:+14155238886',
+        body=ans,
+        to='whatsapp:+5214426778033'
     )
     print(message.sid)
     # Return an HHTPResponse as Django expects a response from the view
     return HttpResponse(status=200)
 
+def parseInput(request):
+    body_unicode = request.body.decode('utf-8')
+    unq = unquote(body_unicode)
+    
+    pfrom = ""
+    #check id client
+    if request:
+        ans=str(unq)
+        pfrom = str(ans)
+    else:
+        ans='source'
+    return pfrom
+
+@csrf_exempt
+def bot(request):
+    ans = parseInput(request)    
+    botReply(ans)
+    
+    # Return an HHTPResponse as Django expects a response from the view
+    return HttpResponse(status=200)
